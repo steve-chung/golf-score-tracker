bdcbf46cb578e85c33cbe882395ab9f56b63242b
Steve Chung <heeman_1@hotmail.com>
2018-10-22 14:10:50 -0700

    #1 added search bar the center the map based upon the selected city

--- src/component/map.js
+++ src/component/map.js
@@ -1,44 +1,34 @@
 import React, {Component} from 'react'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import { connect } from 'react-redux'
 import { setCenter } from '../store/action/map'
 
 class Map extends Component {
-  constructor(props) {
-    super(props)
 
-  }
   componentDidMount() {
 
-    const id = navigator.geolocation.watchPosition(position => {
+    navigator.geolocation.watchPosition(position => {
       this.props.setCenter(position.coords.latitude, position.coords.longitude)
-    //   this.setState({
-    //     lat: position.coords.latitude,
-    //     lng: position.coords.longitude
-    //   })
     }, (err) => {
       console.error(err)
     })
   }
   render() {
     const {lat, lng} = this.props.map
-    // const lat = this.props
-    // const lng = this.props.map.lng
-    console.log(lat)
-    const GoogleMapContainer = withScriptjs(withGoogleMap(props => (
+
+    const GoogleMapContainer = withGoogleMap(props => (
       <GoogleMap
         defaultZoom={12}
         defaultCenter={{ lat, lng }}
       >
         <Marker position={{ lat, lng }} />
       </GoogleMap>
-    )))
+    ))
 
     return (
       <div style={{height: `20rem`}}>
         <GoogleMapContainer
-          googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBras_Muhkliq5FTPdbIDXMgaKFPdZOX58&v=3.exp&libraries=geometry,drawing,places'
-          loadingElement={<div style={{ heigh: `100%`}}/>}
+          loadingElement={<div style={{ heigh: `100%` }}/>}
           containerElement={<div style={{height: `100%`}}/>}
           mapElement={<div style={{height: `100%`}}/>}
         />
--- src/container/app.js
+++ src/container/app.js
@@ -1,20 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 import Map from '../component/map'
+import {TextField, List, ListItem} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng, loading
+} from 'react-places-autocomplete'
+import {connect} from 'react-redux'
+import {setCenter} from '../store/action/map'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  add: {
+    float: 'right',
+    height: 80
+  }
+})
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      address: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.hanleSelect = this.handleSelect.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      address: e
+    })
+  }
+
+  handleSelect(e) {
+    geocodeByAddress(e)
+      .then(res => getLatLng(res[0]))
+      .then(latLng => {
+        this.props.setCenter(latLng.lat, latLng.lng)
+      })
   }
 
   render() {
-
+    const { classes } = this.props
+    const renderFunc = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
+      <div className="autocomplete-root">
+        <TextField label='search city' placeholder='search' className={classes.textField}
+          fullWidth InputLabelProps={{ shrink: true }} {...getInputProps()} />
+        <List componet='nav' className="autocomplete-dropdown-container">
+          {loading && <div>Loading...</div>}
+          {suggestions.map((suggestion, i) => (
+            <ListItem key={i} button component='nav' {...getSuggestionItemProps(suggestion)}>
+              <span>{suggestion.description}</span>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
     return (
-      <Map style={{height: `20rem`}}>
-      </Map>
+      <div className='container'>
+        <PlacesAutocomplete value={this.state.address}
+          onChange={this.handleChange} onSelect={this.hanleSelect} >
+          {renderFunc}
+        </PlacesAutocomplete>
+
+        <Map style={{height: `20rem`}}>
+        </Map>
+
+      </div>
+
     )
   }
 }
 
-export default App
+export default connect(null, {setCenter})(withStyles(styles)(App))
