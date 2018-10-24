818e777c8a00cf3163726ffdd534937b667563f5
Steve Chung <heeman_1@hotmail.com>
2018-10-24 10:55:21 -0700

    #1 changed the yelp key and add it into .env file

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const yelp = require('yelp-fusion')
 const app = server.create()
 const middleware = server.defaults()
 const router = server.router('db.json')
-const client = yelp.client('8Tlqjmp9Pgh0iewmK3vrv8rZ4h8ORn6mOwHWdSSe76WTehbIpdY_DdewsZY6gja0kGcvLOGGB4qVUJQkkaOp2-DCiJBMxAH_0Nk-pJ9WcS26UrMkbFsLae2FnsDKW3Yx')
-
+const client = yelp.client(process.env.YELPKEY)
 app.use(middleware)
 
 app.use('/api/position', (req, response, next) => {
