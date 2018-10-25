25814328b42ebc34219d277d4643516f8c0db7e5
Steve Chung <heeman_1@hotmail.com>
2018-10-25 10:25:08 -0700

    #3 created a invite component

--- src/container/app.js
+++ src/container/app.js
@@ -14,6 +14,7 @@ import PlacesAutocomplete, {
   getLatLng
 } from 'react-places-autocomplete'
 import CourseList from '../component/courseList'
+// import Invite from './invite'
 
 const styles = theme => ({
   container: {
---
+++ src/container/invite.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Invite extends Component {
+  // constructor() {
+  //   super()
+  // }
+
+  render() {
+
+    return (
+      <div>
+        I am here!!!
+      </div>
+    )
+  }
+}
+
+export default Invite
