274efc0c737acd6de51c25bb90a824ddba96d77d
Steve Chung <heeman_1@hotmail.com>
2018-10-29 21:33:14 -0700

    #8 updated players state with last course players

--- src/container/score.js
+++ src/container/score.js
@@ -4,20 +4,30 @@ class Score extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      players: []
     }
   }
 
   componentDidMount() {
     fetch('/data/history', {method: 'GET'})
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => {
+        let newPlayers = [ ]
+        const lastCourse = res.length - 1
+        for (let i = 0; i < res[lastCourse].players.length; i++) {
+          newPlayers.push(res[lastCourse].players[i])
+        }
+        this.setState({
+          players: newPlayers
+        })
+      })
       .catch(err => {
         console.error(err)
       })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>Score</h1>
