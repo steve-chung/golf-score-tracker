c83598f6614549894a862c1fc62a35b8e331b4f7
Steve Chung <heeman_1@hotmail.com>
2018-11-03 17:54:08 -0700

    19 created history list component with collapsible list, fix the date to correctly displayed

--- src/container/history.js
+++ src/container/history.js
@@ -1,60 +1,47 @@
 import React, { Component } from 'react'
+import HistoryList from '../component/historyList'
 
 class History extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      history: null
     }
   }
   componentDidMount() {
+    const date = Date.now()
     fetch('/data/games', {method: 'GET'})
       .then(res => res.json())
       .then(res => {
-        // let history = []
-        // let players = []
         let playerList = []
         let courseObj = {}
-        // for (let i = 1; i < res.length; i++) {
-        //   for (let j = 0; j < res[i].players.length; j++) {
-        //     console.log(res[i].players[j])
-        //     playerObj = {}
-        //     playerObj.id = res[i].players[j].id
-        //     playerObj.name = res[i].players[j].name
-        //     playerObj.totalScore = res[i].players[j].totalScore
-        //     players.push(playerObj)
-        //     console.log(players)
-        //   }
-        //   courseObj.date = res[i].date
-        //   courseObj.course = res[i].course
-        //   courseObj.players = players
-        //   console.log(courseObj)
-        //   history.push(courseObj)
-        //   console.log(history)
-        //   players = [ ]
-        // }
-        const history = res.map(course => {
+        const history = res.filter(coures => (
+          coures.date < date)).map(course => {
           console.log(courseObj)
           playerList = course.players.map(player => {
             return Object.assign({}, {id: player.id, name: player.name, totalScore: player.totalScore})
-            // playerObj.id = player.id
-            // playerObj.name = player.name
-            // playerObj.totalScore = player.totalScore
-            // return playerObj
           })
           courseObj = Object.assign({},
             {id: course.id, date: course.date, name: course.course, player: playerList})
-
           return courseObj
         })
         console.log(history)
+        this.setState({
+          history
+        })
+      })
+      .catch(err => {
+        console.error(err)
       })
   }
 
   render() {
+    const {history} = this.state
+    console.log(history)
     return (
-      <div>
-        I am history!!
+      <div className='container' style={{width: '80%', margin: 'auto'}}>
+        <h1 className='title'>Course History</h1>
+        <HistoryList history={history} />
       </div>
     )
   }
