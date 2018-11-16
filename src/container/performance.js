f2fe88eeaa1565e0fae8c6f5cc887cc41f9753dd
Steve Chung <heeman_1@hotmail.com>
2018-11-16 13:38:31 -0800

    #21 created a loop to collect the data

--- src/container/performance.js
+++ src/container/performance.js
@@ -5,6 +5,56 @@ class Performance extends Component {
   //   super(props)
   // }
 
+  componentDidMount() {
+    const date = Date.now()
+    let dataObj = {}
+    dataObj.puttsGreen = 0
+    fetch('/data/games', {method: 'GET'})
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        const history = res.filter(coures => (
+          coures.date < date)).map(course => {
+          console.log(course)
+          let courseObj = {}
+          const scoreStat = course.players[0].hole.map(
+            hole => {
+              console.log(hole)
+              if (!dataObj[hole.firstClub]) {
+                dataObj[hole.firstClub] = +hole.firstDistance
+              }
+              else {
+                dataObj[hole.firstClub] += +hole.firstDistance
+              }
+              if (!dataObj[hole.secondClub]) {
+                dataObj[hole.secondClub] = +hole.secondDistance
+              }
+              else {
+                dataObj[hole.secondClub] += +hole.secondDistance
+              }
+              dataObj.puttsGreen += +hole.stroksGreen
+              console.log(dataObj)
+            })
+          courseObj = Object.assign({},
+            {id: course.id,
+              date: course.date,
+              playerName: course.players[0].name,
+              scores: +course.players[0].totalScore,
+              scoreStats: scoreStat})
+          console.log(courseObj)
+          return courseObj
+        })
+
+        console.log(history)
+
+      //   this.setState({
+      //     history
+      //   })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
   render() {
     return (
       <div>
