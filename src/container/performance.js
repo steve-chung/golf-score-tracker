37f4f1ae62fb1f9f01066efd3cc88e8d7e42c2c0
Steve Chung <heeman_1@hotmail.com>
2018-11-16 17:12:11 -0800

    #21 added a count of clubs used into the data

--- src/container/performance.js
+++ src/container/performance.js
@@ -7,41 +7,42 @@ class Performance extends Component {
 
   componentDidMount() {
     const date = Date.now()
-    let dataObj = {}
-    dataObj.puttsGreen = 0
     fetch('/data/games', {method: 'GET'})
       .then(res => res.json())
       .then(res => {
-        console.log(res)
         const history = res.filter(coures => (
           coures.date < date)).map(course => {
-          console.log(course)
           let courseObj = {}
-          const scoreStat = course.players[0].hole.map(
-            hole => {
-              console.log(hole)
-              if (!dataObj[hole.firstClub]) {
-                dataObj[hole.firstClub] = +hole.firstDistance
-              }
-              else {
-                dataObj[hole.firstClub] += +hole.firstDistance
-              }
-              if (!dataObj[hole.secondClub]) {
-                dataObj[hole.secondClub] = +hole.secondDistance
-              }
-              else {
-                dataObj[hole.secondClub] += +hole.secondDistance
-              }
-              dataObj.puttsGreen += +hole.stroksGreen
-              console.log(dataObj)
-            })
+          let initObj = {}
+          const scoreStat = course.players[0].hole.reduce((acc, hole) => {
+            let count = 0
+            if (!acc[hole.firstClub]) {
+              acc[hole.firstClub] = +hole.firstDistance
+              acc.count = Object.assign(initObj, {[hole.firstClub]: 1})
+            }
+            else {
+              acc[hole.firstClub] += +hole.firstDistance
+              count = acc.count[hole.firstClub] + 1
+              acc.count = Object.assign(acc.count, {[hole.firstClub]: count})
+            }
+            if (!acc[hole.secondClub]) {
+              acc[hole.secondClub] = +hole.secondDistance
+              acc.count = Object.assign(initObj, {[hole.secondClub]: 1})
+            }
+            else {
+              count = acc.count[hole.secondClub] + 1
+              acc[hole.secondClub] += +hole.secondDistance
+              acc.count = Object.assign(acc.count, {[hole.secondClub]: count})
+            }
+            acc.puttsGreen += +hole.stroksGreen
+            return acc
+          }, {puttsGreen: 0})
           courseObj = Object.assign({},
             {id: course.id,
               date: course.date,
               playerName: course.players[0].name,
               scores: +course.players[0].totalScore,
               scoreStats: scoreStat})
-          console.log(courseObj)
           return courseObj
         })
 
